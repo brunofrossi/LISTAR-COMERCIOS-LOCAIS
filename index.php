f24c226027e73e69da5a32896e7750dd6ad33062
Bruno Fonseca Rossi <brunofrossi@gmail.com>
2020-04-10 01:18:44 -0300

    erro no menu
    erro no menu do individual

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 </head>
 <body>
 
+
 <div class="d-flex" id="wrapper">
     <!-- menu -->
     <?php
--- individual.php
+++ individual.php
@@ -17,27 +17,12 @@
 <body>
 
 <div class="d-flex" id="wrapper">
-    <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
-    <img src="assets/logoif.png" />
-      <div class="sidebar-heading cabecalho-titulo text-center">Agenda Telefônica<p class="text-right p-cadastro"><a class="btn btn-danger link-cadastro" href="./sis_admin/index.php">Cadastre-se</a></p></div>
-      <div class="list-group list-group-flush">
+    <!-- menu -->
+    <?php
+            include 'menu.php';
+    ?>
+    <!-- /menu -->
 
-        <?php
-            //buscar segmentos
-            $sql = "SELECT * FROM segmento ORDER BY nome";
-            $resultado= $conexao->query($sql);
-
-            while($linha=$resultado->fetch_array()){
-                $link = "index.php?id=".$linha['idsegmento'];
-                $nome = $linha['nome'];
-                echo "<a href='$link' class='list-group-item list-group-item-action bg-light'>$nome</a>";
-            }
-        ?>
-
-      </div>
-    </div>
-    <!-- /sidebar -->
 
     <!-- Page Content -->
     <div id="page-content-wrapper">
