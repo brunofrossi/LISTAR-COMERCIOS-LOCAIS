c4247ce80e2ec497ade3fab95e71bfbec5b0fee5
Bruno Fonseca Rossi <brunofrossi@gmail.com>
2020-04-10 02:12:50 -0300

    new adm

--- sis_admin/menu.php
+++ sis_admin/menu.php
@@ -5,6 +5,21 @@
             <div class="list-group list-group-flush">
                 <a href="systemHome.php" class='list-group-item list-group-item-action bg-light'>Início</a>
                 <a href="newClient.php" class='list-group-item list-group-item-action bg-light'>Cadastrar</a>
-            </div>
+ 
+<?php
+    //buscar pelo tipo do usuario 
+    $sql = "SELECT `servidor` FROM `usuario` WHERE `idusuario`= $id and servidor=1 ";
+            $resultado= $conexao->query($sql);
+            if($resultado->num_rows>0){
+                ?>
+                    <a href="activate.php" class='list-group-item list-group-item-action bg-light'>Ativar Cadastro</a>
+                    <a href="mural.php" class='list-group-item list-group-item-action bg-light'>Mural</a>
+                <?php
+            }
+        ?>
+        </div>
     </div>
+
+
+
 <!-- /Menu -->
---
+++ sis_admin/mural.php
@@ -0,0 +1,111 @@
+<?php
+    include '../conexao.php';
+    session_start();
+if((!isset ($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true))
+{
+  unset($_SESSION['email']);
+  unset($_SESSION['senha']);
+  header('location:index.php');
+  }
+
+  $id = $_SESSION['id'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Telefônica</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="css/simple-sidebar.css" rel="stylesheet" id="sidebar-css">
+    <link href="css/style.css" rel="stylesheet" id="style">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+</head>
+<body>
+
+<div class="d-flex" id="wrapper">
+   <!-- Menu -->
+   <?php
+    include("menu.php");
+    ?>
+    <!-- /Menu -->
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+    
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom bg-sup">
+            <button class="btn btn-primary bt-menu" id="menu-toggle">
+                <img src="icons/list.svg" alt="" width="24" height="24" title="Facebook">
+            </button>
+            <div class="d-flex justify-content-end sair-bt">
+                <a href="sair.php" class="btn btn-danger">Sair</a>
+            </div>
+        </nav>
+
+      <div class="container-fluid">
+            <div class="row">
+                <div class="espaco"> &nbsp; </div>
+
+                
+                <!--CARD-->
+                <div class="col-xs-12 col-md-10 offset-md-1">
+                    <!--<div class="image-flip">
+                        <div class="mainflip">
+                            <div class="frontside">-->
+                               
+                                <!--Listando os cadastros-->
+                                <div calss="table-responsive">
+                                    <table class="table table-hover">
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>Telefone</th>
+                                        <th>Segmento</th>
+                                        <th> </th>
+                                    </tr>
+                                    <?php
+                                        //Exibir os itens gravados
+                                        $sql = "SELECT * FROM comercio c 
+                                        INNER JOIN segmento s 
+                                        ON  c.segmento_idsegmento = s.idsegmento
+                                        WHERE usuario_idusuario = $id;";
+                                        $resultado= $conexao->query($sql);
+                                        while($linha=$resultado->fetch_array()){
+                                            echo "<tr>";
+                                            echo "<td>".$linha["nome_fantasia"]."</td>";
+                                            echo "<td>".$linha["telefone"]."</td>";
+                                            echo "<td>".$linha["nome"]."</td>";
+                                            echo "<td><a href='alterClient.php?id=".$linha["idcomercio"]."'><img src='icons/pencil.svg' alt='Alterar' title='Alterar'> Alterar</a></td>";
+                                            echo "</tr>";
+                                        }
+                                    ?>
+                                    </table>
+                                </div>
+                                <!-- ./Listando os cadastros -->
+
+                            <!-- </div>
+                        </div>
+                    </div> -->
+                </div>
+                <!--./CARD-->
+                
+                <div class="espaco"> &nbsp; </div>
+            </div>
+            
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+</div>
+
+
+<!--Script-->
+<script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
+
+</body>
+</html>
