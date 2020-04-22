7b8ff757aff851215808f9d5d48f5d9b8bbcefcf
Diego Rossi <diego.rossi@yahoo.com.br>
2020-04-22 00:01:29 -0300

    Alterando o rodapé do admin

--- sis_admin/rodape.php
+++ sis_admin/rodape.php
@@ -1,33 +1,30 @@
-<footer>
+<footer class="fixed-bottom">
    <div class="container" >
-            <div class="row" >
+            <div class="row align-middle" >
                 <div class="col-sm-3" >
-                    <ul >
-                        <li > <img src="../assets/logoifkta.png" width="25%" alt="Imagem de capa do card"></li>
-                        <li > <img  src="../assets/logoifkta.png" width="25%" alt="Imagem de capa do card"></li>
+                    <ul class="item-rodape">
+                        <li class="d-inline"> <img src="../assets/logoifkta.png" alt="Imagem de capa do card"></li>
+                        <li class="d-inline"> <img  src="../assets/logoifkta.png" alt="Imagem de capa do card"></li>
                     </ul>
                 </div>
                 <div class="col-sm-3">
-                    <ul >
-                        <li ><a href="http://ktaemcasa.com.br/">Inicio</a></li>
-                        <li ><a href="http://ktaemcasa.com.br/sis_admin/index.php" >Cadastrar no sistema</a></li>
+                    <ul class="item-rodape">
+                        <li ><a href="http://ktaemcasa.com.br/" class="small">Inicio</a></li>
+                        <li ><a href="http://ktaemcasa.com.br/sis_admin/index.php" class="small">Cadastrar no sistema</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3">
-                    <ul >
-                        <li ><a href="https://www.ifsudestemg.edu.br/" target="_BLANK">Sobre-nós</a></li>
-                        <li ><a href="">Informações do Projeto de Extensão</a></li>
+                    <ul class="item-rodape">
+                        <li ><a href="https://www.ifsudestemg.edu.br/" target="_BLANK" class="small">Sobre-nós</a></li>
+                        <li ><a href="" class="small">Informações do Projeto de Extensão</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-3" >
+                <div class="col-sm-3 text-center" >
                     <div class="social-networks">
                         <a target="_BLANK" href="https://www.facebook.com/ifsudestemgcataguases/"> <img src="icons/facebook.svg" width="10%" >   </a>
                         <a target="_BLANK" href="https://www.instagram.com/ifsudestemgcataguases/">  <img src="icons/instagram.svg" width="10%">  </a>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="footer-copyright" >
-            <p>© 2020 Copyright</p>
         </div>
  </footer>
